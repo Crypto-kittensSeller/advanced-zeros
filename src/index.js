8d8296d008e33c92cb9effedb963c5955b31419a
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2018-10-01 12:08:52 +0300

    advanced zeros counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let minResult = 1e15;
+  for (let i = 2; i<=base; i++ ){
+      if (base % i === 0){
+        let counterBase = 0;
+        while(base % i === 0){
+          counterBase++;
+          base /=i;
+        }
+        let counterNum = 0;
+        let x = number;
+        while ( x / i >= 1){
+          counterNum += Math.floor(x / i);
+          x /= i;
+        }
+        minResult = Math.min(minResult, Math.floor(counterNum / counterBase));
+      }
+  }
+return minResult;
 }
